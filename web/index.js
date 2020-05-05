1dc774570c519eb0ee47581766a3eb94aa2b7cc7
Mauricio Bustos <m@bustos.org>
2020-05-05 11:15:05 -0700

    formatting and height

--- web/index.js
+++ web/index.js
@@ -1,3 +1,16 @@
+/*
+    Copyright (C) 2020 Mauricio Bustos (m@bustos.org)
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 var socket = io();
 
@@ -24,6 +37,10 @@ $('#submitPoof').submit(function (e) {
 
 socket.on('poof', function(msg){
     console.log('broadcast ' + JSON.stringify(msg));
-    command = msg['name'] + ' poofed ' + msg['count'] + ' times\n';
+    var time = new Date();
+    var hour = time.getHours();
+    var minutes = time.getMinutes();
+    console.log('broadcast ' + JSON.stringify(msg));
+    command = hour + ':' + minutes + ' - ' + msg['name'] + ' poofed ' + msg['count'] + ' times\n';
     $('#queueText')[0].prepend(command);
 });
